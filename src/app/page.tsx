c5ff52c0e373fd6fb9b1e548b7b6f69ca887bfca
HanzPo <hanznathanpo@gmail.com>
2025-02-17 21:43:07 -0500

    Migrated old website to next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hi! I&apos;m Hanz Po</h1>
+      <p>I&apos;m a Computer Science student at the University of Waterloo with an interest in data science and machine learning. I also enjoy learning about finance, economics, urban planning, and history in my spare time.</p>
+
+      <p>As you can tell by the lack of CSS, this site is still under construction.</p>
+
+      <p>Here are some links that may be useful:</p>
+      <ul>
+        <li><a href="/files/Resume___January_2025_V6.pdf">Resume</a></li>
+        <li><a href="https://www.github.com/HanzPo">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/hanznathanpo/">Linkedin</a></li>
+        <li><a href="https://x.com/hanznathanpo">Twitter</a></li>
+        <li><a href="https://www.instagram.com/hanznathanpo">Instagram</a></li>
+        <li><a href="pages/old.html">Old website</a></li>
+      </ul>
+    </div>
+  );
+}
