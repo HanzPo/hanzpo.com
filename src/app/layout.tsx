3073735bee214444971fafd88cf02077271100d1
HanzPo <hanznathanpo@gmail.com>
2025-09-09 12:29:33 -0400

    Add webring shit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Image from 'next/image';
 import { Crimson_Pro, Lora } from 'next/font/google';
 
 const crimsonPro = Crimson_Pro({ 
@@ -38,6 +39,21 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${crimsonPro.className} ${lora.className} background-image text-gray-200`}>
         {children}
+        <footer className="py-8 flex justify-center">
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <a href='https://cs.uwatering.com/#https://hanzpo.com?nav=prev' aria-label="Previous site in CS Webring">←</a>
+            <a href='https://cs.uwatering.com/#https://hanzpo.com' target='_blank' rel="noopener noreferrer" aria-label="CS Webring">
+              <Image
+                src='https://cs.uwatering.com/icon.white.svg'
+                alt='CS Webring'
+                width={24}
+                height={24}
+                style={{ opacity: 0.8 }}
+              />
+            </a>
+            <a href='https://cs.uwatering.com/#https://hanzpo.com?nav=next' aria-label="Next site in CS Webring">→</a>
+          </div>
+        </footer>
       </body>
     </html>
   );
